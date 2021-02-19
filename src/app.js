c555087276dc6a869935c77bb406afae63ba29ef
Andrew Schmit <schmita91@gmail.com>
2021-02-19 17:37:07 -0500

    working server

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
 
 const fetchRouter = require('./router/fetchRouter');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(fetchRouter);
 
 
--- src/router/fetchRouter.js
+++ src/router/fetchRouter.js
@@ -1,86 +1,108 @@
 const express = require('express');
 const fetchRouter = express.Router();
 
-const STORE = [];
+// Create empty userPoints object to store payers and point balances.
+userPoints = {};
+// Create empty transaction history array to store transaction records.
+transactionHistory = [];
 
 fetchRouter
     .route("/addPoints") 
     .post((req, res) => {
-        const newRecord = req.body;
-        if(newRecord.points <= 0) {
-          res.status(400).send("Cannot add 0 or negative points")
-        }
-        const date = new Date(Date.now());
-        const timeStamp = date.toISOString();
-        const record = { ...newRecord, timestamp: timeStamp };
-        STORE.push(record);
-        console.log(record)
-        res.status(201).send(`Successfully added ${record.points} points.`);
-  });
+      transaction = req.body;
+      // Create timestamps for new transactions.
+      transaction.time= new Date(Date.now())
+      transaction.time.toISOString();
+
+      // Add transaction record to transaction history.
+      transactionHistory.push(transaction);
+      if(userPoints[transaction.payer]) {
+        userPoints[transaction.payer]+=transaction.points;
+      } else {
+        userPoints[transaction.payer]=transaction.points;
+      }
+      res
+        .status(200)
+        .send(`Successfully added ${transaction.points} points from ${transaction.payer}`);  
+    });
 
 
 fetchRouter
     .route("/spend")
-    .post((req, res) => {
-        const points = req.body.points;
-        if(points <= 0) {
-          res.status(400).send("Bad Request: Cannot spend negative points");
-        } else {
-            try {
-              response = spendPoints(points);
-              res.status(200).send(response);
-            } 
-            catch (e) {
-              res.status(400).send("Could not spend points");
-            }
-          }
-      });
-    
-    const spendPoints = (pointsToSpend) => {
-      let spendRequest = pointsToSpend;
-      let spendRecord = [];
-      const l = STORE.length;
-      for (let index = 0; index < l; index++) {
-        console.log(spendRequest);
-        if (spendRequest == 0) {
-          break;
-        }
-        const record = STORE.shift();
-        let payer = record["payer"];
-        let points = record["points"];
-        if (points <= spendRequest) {
-          spendRequest -= points;
-          spendRecord.push({ payer: payer, points: -points });
-        } else {
-          spendRecord.push({ payer: payer, points: -spendRequest });
-          const date = new Date(Date.now());
-          const timeStamp = date.toISOString();
-          STORE.push({
-            payer: payer,
-            points: points - spendRequest,
-            timeStamp: timeStamp,
-          });
-          console.log(STORE);
-          spendRequest = 0;
-        }
+    .post((req,res)=>{
+      let availablePoints = 0;
+      let spendRequest = req.body.points;
+      
+      // Check user point balance and compare to spend request
+      for (var value in userPoints) {
+          availablePoints += userPoints[value];
       }
-      console.log(spendRecord);
-      return [...spendRecord];
-    };
+      if (availablePoints < spendRequest) {
+          res.send("Uh oh! You don't have enough points for this request.");
+      } else {
+      // Sort transaction history by date, so that oldest points will
+      // be spent first. transactionHistory array is transformed by date.
+          transactionHistory = transactionHistory.sort((a,b) => { 
+            return new Date(a.time) - new Date(b.time) 
+          });
+          
+      // Create payer spend object to store response with points deducted
+      // from each payer.
+          payerSpend={};
+      
+      // Store spendRequest in remaining variable
+          remaining = spendRequest;
+
+      // Loop through each transaction in the transaction history, comparing
+      // the spendRequest to the amount of points in each transaction, starting 
+      // with the oldest transaction. If the amount of spend is greater than points,
+      // max out the points and move to the next transaction. If it is less, 
+      // deduct the spend request points and end loop.
+          for(let i = 0; i < transactionHistory.length; i++) {
+            var spend = 0;
+            currentRecord = transactionHistory[i];
+            if(currentRecord.points - remaining >= 0) {
+              spend = remaining;
+              transactionHistory[i].points -= spend;
+            } else {
+              spend = currentRecord.points;
+              transactionHistory[i].points=0;
+            }
+            
+        // Deduct the spent amount from the previous record and continue iterating
+            spend *= parseInt(-1);
+            remaining += spend;
+        // If points have already been deducted from payer, continue deducting.
+              if(payerSpend[currentRecord.payer]) {
+                payerSpend[currentRecord.payer]+= spend;
+              } else { 
+                payerSpend[currentRecord.payer] = spend;
+              }
+              if(remaining <= 0) {  
+                for(var k in userPoints) {
+                  userPoints[k] = 0;
+                }
+                for(var j = 0; j < transactionHistory.length; j++) {
+                  if(transactionHistory[j].points==0) {
+                    transactionHistory.splice(j,1);
+                    i--;
+                  } else if(userPoints[transactionHistory[j].payer]) {
+                      userPoints[transactionHistory[j].payer]+=transactionHistory[j].points;
+                    } else {
+                      userPoints[transactionHistory[j].payer]=transactionHistory[j].points;
+                      }
+                }
+                res.send(payerSpend);
+              }
+          }
+      }
+    });
 
 fetchRouter
     .route("/points")
-    .get((req, res) => {
-        let points = new Map();
-        STORE.forEach((record) => {
-          if (points.has(record["payer"])) {
-            const spendRequest = points.get(record["payer"]);
-            points.set(record["payer"], spendRequest + record["points"]);
-          } else {
-            points.set(record["payer"], record["points"]);
-          }
-        });
-        res.status(200).json(Object.fromEntries(points));
-      });
-
+    .get((req,res)=>{
+      res
+        .send(userPoints);
+    });
+    
 module.exports = fetchRouter;
